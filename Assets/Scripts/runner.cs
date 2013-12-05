bc249f5abe20169ee9f553dbfbd5e0ef9fec5cde
LIAM PAUL DAKIN MA <tutoeth@gmail.com>
2013-12-05 12:14:21 +0000

    3

---
+++ Assets/Scripts/runner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class runner : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+
+    }
+}
